e57edff46ec0cdf28447c6e574fca1a0f5f7647a
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-11-06 07:49:30 +0000

    feat(location): :sparkles: outline functions to return all possible location combinations

--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -8,7 +8,19 @@ class Location(Hass):
         trackers = self.get_trackers()
         for tracker in trackers:
             if tracker == 'device_tracker.gabriella_martin':
-                g_home = True if self.get_state(tracker) == 'home' else False
+                self.g_home = True if self.get_state(tracker) == 'home' else False
             elif tracker == 'device_tracker.lorenzo_speariett':
-                l_home = True if self.get_state(tracker) == 'home' else False
- 
+                self.l_home = True if self.get_state(tracker) == 'home' else False
+    
+    def everyone_home(self):
+        return self.g_home and self.l_home
+    
+    def l_home_g_away(self):
+        return self.l_home and not self.g_home
+    
+    def g_home_l_away(self):
+        return self.g_home and not self.l_home
+    
+    def everyone_away(self):
+        return not (self.g_home and self.l_home)
+
