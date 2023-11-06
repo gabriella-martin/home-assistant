53694a087df0e1348cb4d39b05a53010331fb706
Gabriella Martin <gabriella.martin0101@gmail.com>
2023-11-06 07:50:39 +0000

    revert(location): :fire: remove unnecessary list

--- appdaemon/apps/test.py
+++ appdaemon/apps/test.py
@@ -1,8 +1,5 @@
 from appdaemon.plugins.hass.hassapi import Hass
 
-LIFE_360_TRACKERS = ['device_tracker.gabriella_martin', 
-                     'device_tracker.lorenzo_speariett']
-
 class Location(Hass):
     def initialize(self):
         trackers = self.get_trackers()
